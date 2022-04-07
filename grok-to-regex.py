ba009870e17185cd58ef7ab4b66737ece78ddf45
Malcolm Jones <bossjones@theblacktonystark.com>
2022-04-07 17:23:46 -0400

    Feature: grok to regex

--- grok-to-regex.py
+++ grok-to-regex.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-
+# SOURCE: https://gist.githubusercontent.com/elementalvoid/59afc405f2f5726ad1980e8d8178536b/raw/3e2a6a88ef10acc5d95272cea90cd01a6fa7c6d9/grok-to-regex.py
 import argparse
 import re
 from os import walk
 from os.path import join
+from pygrok import Grok
 
 
 def get_patterns(patterns_dir):
@@ -17,7 +18,12 @@ def get_patterns(patterns_dir):
                         patterns[k] = v.rstrip('\n')
     return patterns
 
-
+def dump_regex(expression, patterns_dir):
+    pattern = expression
+    grok = Grok(pattern, custom_patterns_dir=patterns_dir)
+    # print(f"{grok.regex_obj.pattern}")
+    print(f"{grok.regex_obj.pattern}")
+    
 def convert(expression, patterns):
     groks = re.compile('%{[^}]*}')
 
@@ -57,10 +63,18 @@ def convert(expression, patterns):
 
 if __name__ == '__main__':
     # grok-to-regex.py "%{HOSTPORT}" -d patterns/
+    # (SRC=%{IPV4:firewall.source.ip} DST=%{IPV4:firewall.destination.ip} LEN=%{UNSIGNED_INT:firewall.packet_length} TOS=0x%{BASE16NUM:firewall.tos} PREC=0x%{BASE16NUM:firewall.precidence_field} TTL=%{UNSIGNED_INT:firewall.ttl} ID=%{UNSIGNED_INT:firewall.id}(?:(\s*))?(?:%{WORD:firewall.dont_fragment})?(?:(\s*))?PROTO=%{WORD:firewall.nf_protocol} SPT=%{INT:firewall.spt} DPT=%{INT:firewall.dtp} %{GREEDYDATA:firewall.tcp_opts})
+    #  ./grok-to-regex.py "%{BOSSJONES_IPTABLES_IP_START}"
+    #  ./grok-to-regex.py "%{IPV4}"
+    #  ./grok-to-regex.py "%{UNSIGNED_INT}"
+    #  ./grok-to-regex.py "%{BASE16NUM}"
+    #  ./grok-to-regex.py "%{WORD}"
+    #  ./grok-to-regex.py "%{INT}"
+    #  ./grok-to-regex.py "%{GREEDYDATA}"
     parser = argparse.ArgumentParser()
     parser.add_argument('expression', metavar='expr', help='A grok expression.')
-    parser.add_argument('-d', '--patterns-dir', dest='patterns_dir', default='patterns',
+    parser.add_argument('-d', '--patterns-dir', dest='patterns_dir', default='patterns/legacy',
                         help='Directory to find grok patterns.')
     args = parser.parse_args()
-    patterns = get_patterns(args.patterns_dir)
-    convert(args.expression, patterns)
+    # patterns = get_patterns(args.patterns_dir)
+    dump_regex(args.expression, args.patterns_dir)
